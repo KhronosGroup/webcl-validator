6575f158b91061774cc4f9b4096b8efbe28b177d
Rami Ylimäki <rami.ylimaki@vincit.fi>
2013-08-26 16:18:24 +0300

    Document actions implementing various validation stages.
    Signed-off-by: Rami Ylimäki <rami.ylimaki@vincit.fi>

--- src/WebCLAction.hpp
+++ src/WebCLAction.hpp
@@ -15,27 +15,36 @@ class WebCLReporter;
 class WebCLPreprocessor;
 class WebCLTransformer;
 
+/// Abstract base class for an action performed by a validation stage.
 class WebCLAction : public clang::FrontendAction
 {
 public:
 
+    /// Constructor. If output filename isn't given, standard output
+    /// is used.
     explicit WebCLAction(const char *output = NULL);
     virtual ~WebCLAction();
 
 protected:
 
+    /// Initializes the action state. Full initialization isn't done
+    /// in constructor, because the compiler instance isn't
+    /// necessarily available at construction time.
     virtual bool initialize(clang::CompilerInstance &instance);
 
+    /// Error reporting functionality.
     WebCLReporter *reporter_;
-    // Preprocessing callbacks are needed for both stages. Include
-    // directives are handled during preprocessing and pragmas during
-    // AST parsing.
+    // Preprocessing callbacks may be needed also when AST is
+    // parsed. For example, include directives are handled during
+    // preprocessing and pragmas during AST parsing.
     WebCLPreprocessor *preprocessor_;
-
+    /// Output filename.
     const char *output_;
+    /// Stream corresponding to the output filename.
     llvm::raw_ostream *out_;
 };
 
+/// Performs preprocessing stage only. Doesn't parse AST.
 class WebCLPreprocessorAction : public WebCLAction
 {
 public:
@@ -54,6 +63,7 @@ public:
     virtual bool usesPreprocessorOnly() const;
 };
 
+/// A base class for stages that use AST matchers for consuming ASTs.
 class WebCLMatcherAction : public WebCLAction
 {
 public:
@@ -73,14 +83,22 @@ protected:
     /// \see WebCLAction
     virtual bool initialize(clang::CompilerInstance &instance);
 
+    /// Finds matches from AST.
     clang::ast_matchers::MatchFinder finder_;
+    /// Runs matchers when AST has been parsed.
     clang::ASTConsumer *consumer_;
+    /// Stores transformations.
     clang::Rewriter *rewriter_;
-
+    /// Interface for naming conventions.
     WebCLTransformerConfiguration cfg_;
+    /// Outputs stored transformations.
     WebCLPrinter *printer_;
 };
 
+/// Performs early normalizations:
+///
+/// - Complains about illegal identifiers.
+/// - A name is generated for anonymous and nameless structures.
 class WebCLMatcher1Action : public WebCLMatcherAction
 {
 public:
@@ -93,9 +111,16 @@ public:
 
 private:
 
+    /// \return Whether there are no illegal identifiers in the input.
+    ///
+    /// - Identifiers must not exceed 255 characters.
+    /// - Identifiers reserved for validations may not be used.
     bool checkIdentifiers();
 };
 
+/// Performs late normalizations:
+///
+/// - Structure definitions are separated from variable declarations.
 class WebCLMatcher2Action : public WebCLMatcherAction
 {
 public:
@@ -107,6 +132,8 @@ public:
     virtual void ExecuteAction();
 };
 
+/// Runs memory validation algorithm after normalizations have been
+/// performed.
 class WebCLValidatorAction : public WebCLAction
 {
 public:
@@ -129,9 +156,16 @@ private:
     /// \see WebCLAction
     virtual bool initialize(clang::CompilerInstance &instance);
 
+    /// Traverses back and forth AST nodes after AST has been parsed.
     WebCLConsumer *consumer_;
+    /// Creates transformations.
     WebCLTransformer *transformer_;
+    /// Stores transformations.
     clang::Rewriter *rewriter_;
+    /// Component for generating AST nodes as AST is being parsed. We
+    /// don't really need to create this component ourselves, but if
+    /// we do, using internal clang components, e.g. in order to
+    /// manipulate AST nodes, becomes easier.
     clang::Sema *sema_;
 };
 
