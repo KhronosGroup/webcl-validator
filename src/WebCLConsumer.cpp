636a5fb6b621fa8b83d14daccb53dba36a3967cf
Rami Ylimäki <rami.ylimaki@vincit.fi>
2013-08-29 13:54:53 +0300

    Document AST consumer class.
    Signed-off-by: Rami Ylimäki <rami.ylimaki@vincit.fi>

--- src/WebCLConsumer.cpp
+++ src/WebCLConsumer.cpp
@@ -10,27 +10,26 @@ WebCLConsumer::WebCLConsumer(
     clang::CompilerInstance &instance, clang::Rewriter &rewriter,
     WebCLTransformer &transformer)
     : clang::ASTConsumer()
-    , transformer_(transformer)
     , restrictor_(instance)
     , analyser_(instance)
-    , checkingVisitors_()
+    , visitors_()
     , inputNormaliser_(analyser_, transformer)
     , addressSpaceHandler_(analyser_, transformer)
     , kernelHandler_(analyser_, transformer, addressSpaceHandler_)
     , memoryAccessHandler_(analyser_, transformer, kernelHandler_)
     , printer_(instance, rewriter, analyser_, transformer)
-    , transformingPasses_()
+    , passes_()
 {
     // Push in reverse order.
-    checkingVisitors_.push_front(&analyser_);
-    checkingVisitors_.push_front(&restrictor_);
+    visitors_.push_front(&analyser_);
+    visitors_.push_front(&restrictor_);
     // Push in reverse order.
-    transformingPasses_.push_front(&printer_);
-    transformingPasses_.push_front(&memoryAccessHandler_);
-    transformingPasses_.push_front(&kernelHandler_);
-    transformingPasses_.push_front(&addressSpaceHandler_);
-    transformingPasses_.push_front(&inputNormaliser_);
- }
+    passes_.push_front(&printer_);
+    passes_.push_front(&memoryAccessHandler_);
+    passes_.push_front(&kernelHandler_);
+    passes_.push_front(&addressSpaceHandler_);
+    passes_.push_front(&inputNormaliser_);
+}
 
 WebCLConsumer::~WebCLConsumer()
 {
@@ -38,29 +37,29 @@ WebCLConsumer::~WebCLConsumer()
 
 void WebCLConsumer::HandleTranslationUnit(clang::ASTContext &context)
 {
-    traverse(checkingVisitors_, context);
+    checkAndAnalyze(context);
 
     // Introduce changes in way that after every step we still have
     // correclty running program.
-
+    
     // FUTURE: Add memory limit dependence analysis here (or maybe it
-    // could be even separate visitor). Also value range analysis
-    // could help in many cases. Based on dependence analysis, one
-    // could create separate implementations for different calls if we
-    // know which arguments are passed to function.
+    //         could be even separate visitor). Also value range
+    //         analysis could help in many cases. Based on dependence
+    //         analysis, one could create separate implementations for
+    //         different calls if we know which arguments are passed
+    //         to function.
 
-    traverse(transformingPasses_, context);
+    transform(context);
 
     // FUTURE: Add class, which goes through builtins and creates
     //         corresponding safe calls and adds safe implementations
     //         to source.
 }
 
-void WebCLConsumer::traverse(Visitors &sequence, clang::ASTContext &context)
+void WebCLConsumer::checkAndAnalyze(clang::ASTContext &context)
 {
     clang::TranslationUnitDecl *decl = context.getTranslationUnitDecl();
-
-    for (Visitors::iterator i = sequence.begin(); i != sequence.end(); ++i) {
+    for (Visitors::iterator i = visitors_.begin(); i != visitors_.end(); ++i) {
         // There is no point to continue if an error has been reported.
         if (!hasErrors(context)) {
             WebCLVisitor *visitor = (*i);
@@ -69,9 +68,9 @@ void WebCLConsumer::traverse(Visitors &sequence, clang::ASTContext &context)
     }
 }
 
-void WebCLConsumer::traverse(Passes &sequence, clang::ASTContext &context)
+void WebCLConsumer::transform(clang::ASTContext &context)
 {
-    for (Passes::iterator i = sequence.begin(); i != sequence.end(); ++i) {
+    for (Passes::iterator i = passes_.begin(); i != passes_.end(); ++i) {
         // There is no point to continue if an error has been reported.
         if (!hasErrors(context)) {
             WebCLPass *pass = (*i);
@@ -82,6 +81,6 @@ void WebCLConsumer::traverse(Passes &sequence, clang::ASTContext &context)
 
 bool WebCLConsumer::hasErrors(clang::ASTContext &context) const
 {
-   clang::DiagnosticsEngine &diags = context.getDiagnostics();
-   return diags.hasErrorOccurred() || diags.hasUnrecoverableErrorOccurred();
+    clang::DiagnosticsEngine &diags = context.getDiagnostics();
+    return diags.hasErrorOccurred() || diags.hasUnrecoverableErrorOccurred();
 }
--- src/WebCLConsumer.hpp
+++ src/WebCLConsumer.hpp
@@ -7,6 +7,9 @@
 
 #include "clang/AST/ASTConsumer.h"
 
+/// Executes various validation passes that check the AST for errors,
+/// analyze the AST and generate transformations based on the
+/// analysis.
 class WebCLConsumer : public clang::ASTConsumer
 {
 public:
@@ -14,35 +17,42 @@ public:
     explicit WebCLConsumer(
         clang::CompilerInstance &instance, clang::Rewriter &rewriter,
         WebCLTransformer &transformer);
-    ~WebCLConsumer();
+    virtual ~WebCLConsumer();
 
+    /// Runs passes in two stages. The first stage checks errors and
+    /// analyzes the AST. The second stage runs transformation passes.
+    ///
     /// \see ASTConsumer::HandleTranslationUnit
     virtual void HandleTranslationUnit(clang::ASTContext &context);
 
 private:
 
-    typedef std::list<WebCLVisitor*> Visitors;
-    typedef std::list<WebCLPass*> Passes;
+    /// Runs all error checking and analysis passes.
+    void checkAndAnalyze(clang::ASTContext &context);
+    /// Runs all transformation passes.
+    void transform(clang::ASTContext &context);
 
-    void traverse(Visitors &sequence, clang::ASTContext &context);
-    void traverse(Passes &sequence, clang::ASTContext &context);
-
+    /// \return Whether previous passes have reported errors.
     bool hasErrors(clang::ASTContext &context) const;
 
-    WebCLTransformer &transformer_;
-
-    /// Checking visitors.
+    /// Checks AST for WebCL restrictions.
     WebCLRestrictor restrictor_;
+    /// Analyzes AST for transformation passes.
     WebCLAnalyser analyser_;
-    Visitors checkingVisitors_;
+    /// Visitors that check the AST for errors or perform analysis for
+    /// transformation passes.
+    typedef std::list<WebCLVisitor*> Visitors;
+    Visitors visitors_;
 
-    /// Transforming passes.
+    /// Transformation passes.
     WebCLInputNormaliser inputNormaliser_;
     WebCLAddressSpaceHandler addressSpaceHandler_;
     WebCLKernelHandler kernelHandler_;
     WebCLMemoryAccessHandler memoryAccessHandler_;
     WebCLValidatorPrinter printer_;
-    Passes transformingPasses_;
+    /// Passes that generate transformations based on analysis.
+    typedef std::list<WebCLPass*> Passes;
+    Passes passes_;
 };
 
 #endif // WEBCLVALIDATOR_WEBCLCONSUMER
